6668a005a4a81d2762a4c4803d939f63c56d5586
chanyoungcoding <white1614@naver.com>
2023-08-15 23:18:20 +0900

    경로 수정

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About';
+import Common from './pages/Common'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
+        <Route path='/' element={<Common/>}>
+          <Route path='home' element={<Home/>}/>
+          <Route path='about' element={<About/>}/>
+        </Route>
       </Routes>
 
     </>
